4cde7a6271c61db5501b5ca0b8043cb6db48b789
atul123git <atulchandane95@gmail.com>
2022-12-22 12:06:00 +0530

    Code Updated

---
+++ MyGitProject/src/com/projects/Same1.java
@@ -0,0 +1,14 @@
+package com.projects;
+
+import java.util.ArrayList;
+
+public class Same1 {
+public static void main(String args[]) {
+	ArrayList<Integer> al=new ArrayList<Integer>();
+	al.add(90);
+	al.add(10);
+	al.add(50);
+	al.add(20);
+	System.out.println(al);
+}
+}
